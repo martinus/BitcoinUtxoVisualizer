3e5b2c9bad695c37d18780a182706acff38792f3
Martin Ankerl <martin.ankerl@gmail.com>
2020-12-13 09:51:40 +0100

    cleanup: removed deprecated code, moved README.md

--- _old/create_video.bat
+++
@@ -1,7 +0,0 @@
-:: see https://trac.ffmpeg.org/wiki/Encode/H.264
-::
-:: creates a losslessly compressed x264 video. It is quite compact
-:: as only few pixels change from frame to frame.
-3rdparty\ffmpeg -r 60 -i img\%%08d.png -c:v libx264 -preset veryslow -crf 0 utxo.mp4
-
-pause
